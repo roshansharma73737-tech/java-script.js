9f319aa50c496d8a3c8b1fec0f44347a3130613a
Sharma Roshan <roshansharma73737@gmail.com>
2026-07-27 19:03:59 +0530

    Initial project setup

---
+++ frontend/index.js
@@ -0,0 +1,22 @@
+console.log("Roshan")
+let  a = 32;
+console.log(a,"roshan");
+
+const b = "roshan";
+console.log("hello",b);
+console.log("mahi");
+const t = 45;
+console.log(4 + t);
+console.log(typeof t)
+
+let age = 18;
+if(age <= 18){
+console.log("<--you can drive-->")
+}
+else{
+    console.log("you can not dreive")
+}
+
+
+
+let i = 10;
